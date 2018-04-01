3996b1017554bee728627f16f68476d9b6b4374b
VuDucDo <36022384+VuDucDo@users.noreply.github.com>
2018-04-01 22:12:03 +0700

    update

--- TongHop/MTAWEB/MTAWEB/Models/DAO/newsDAO.cs
+++ TongHop/MTAWEB/MTAWEB/Models/DAO/newsDAO.cs
@@ -13,5 +13,26 @@ namespace MTAWEB.Models.DAO
         {
             return model.NEWS.ToList();
         }
+        public NEWS getNew(int id)
+        {
+            return model.NEWS.Find(id);
+        }
+
+        public List<NEWS> getListNewsSub(int idSub)
+        {
+
+            List<NEWS> lsAllNew = model.NEWS.ToList();
+            List<NEWS> lsNewSub = new List<NEWS>();
+            for (int i = 0; i < lsAllNew.Count(); i++)
+            {
+                if (lsAllNew[i].idSubCategory == idSub)
+                {
+                    lsNewSub.Add(lsAllNew[i]);
+                }
+            }
+            return lsNewSub;
+            
+        }
+
     }
 }
